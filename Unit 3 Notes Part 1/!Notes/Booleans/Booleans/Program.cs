e7576beb088a47a5b773584c66c2b76a8c91a2a9
Justin J. Decru <61435320+Justin787@users.noreply.github.com>
2021-01-23 19:17:59 -0500

    Add files via upload

---
+++ Unit 3 Notes Part 1/!Notes/Booleans/Booleans/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/*
+*Written by Justin Decru
+*I don't care if you plagiarize and take credit for my work, as long as I get cool points
+*Check out my Github: https://github.com/Justin787
+*/
+namespace Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring bool
+            bool var1, var2;
+            var1 = 7 > 5;
+            var2 = (3 - 8) >= -50;
+            Console.WriteLine("7>5: " + var1);
+            Console.WriteLine("Other: " + var2);
+
+            //wihtout a variable
+            Console.WriteLine("2+2 == 5 ? " + ((2 + 2) == 5));
+            Console.WriteLine("7 != 8 * 2 " + (7 != 8 * 2));
+
+            // AND = && evals true only if both are tru
+            // OR = || evals if either are tru
+            // XOR = ^ evals if one is tru and one is false
+            Console.WriteLine();
+            Console.WriteLine("true AND true " + (true && true));
+            Console.WriteLine("true AND false " + (true && false));
+            Console.WriteLine("false AND false " + (false && false));
+            Console.WriteLine();
+
+            Console.WriteLine("true OR true " + (true || true));
+            Console.WriteLine("true OR false " + (true || true));
+            Console.WriteLine("false OR false " + (true || true));
+            Console.WriteLine();
+
+            Console.WriteLine("true XOR true " + (true ^ true));
+            Console.WriteLine("true XOR false " + (true ^ false));
+            Console.WriteLine("false XOR false " + (false ^ false));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Unit 3 Notes Part 1/!Notes/If - else/if - else/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+/*
+*Written by Justin Decru
+*I don't care if you plagiarize and take credit for my work, as long as I get cool points
+*Check out my Github: https://github.com/Justin787
+*/
+namespace if___else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//get input from user
+			Console.Write("How much money ya got?");
+			string money = Console.ReadLine();
+			double dmoney = Convert.ToDouble(money);
+
+			//if-else logic
+			if (dmoney >= 100)
+			{
+				Console.WriteLine("You ballin'");
+			}
+			else
+			{
+				Console.WriteLine("LOL u poor");
+			}
+
+			//if-else-if logic
+			Console.WriteLine(" Name a color:  ");
+			string color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.WriteLine("yeah, like BLOOD");
+
+			}
+
+			else if (color == "blue")
+			{
+				Console.WriteLine("Blue LIke the SEA");
+
+			}
+
+			else if (color == "green")
+			{
+				Console.WriteLine("hmmmm...      trees are green");
+
+			}
+
+			else
+			{
+				Console.WriteLine("Thats a boring color");
+			}
+		}
+    }
+}
